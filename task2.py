4ea8e2e04b0bee083df298a478c37bec15116f49
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-11-24 00:17:37 +0900

    task3まで実装

--- task2.py
+++ task2.py
@@ -6,8 +6,6 @@ import csv
 #N回以上連続してタイムアウトした場合にのみ故障とみなすように、設問1のプログラムを拡張せよ。
 #Nはプログラムのパラメータとして与えられるようにすること。
 
-_date="";_ip="";
-        
 target_file=input("読み込みファイルを入力してください⇒ ")
 target_N=int(input("故障判定回数Nを入力してください⇒ "))
 
@@ -15,32 +13,38 @@ with open(target_file) as f:
     records = csv.reader(f)
     records = sorted(records, key=lambda x:(x[1],x[0]))
     
-    _date="";_ip="";_N=0
+    _ip="";_fail_date="";_N=0;
     
     for record in records:
-        #　直前ログでサーバー故障している場合
-        if(_ip!=""):
-            if(record[1]!=_ip):
-                if(target_N<=_N):
-                    print("故障サーバー:",_ip,", 故障中")
-                _date=""
-                _ip=""
-                _N=0
-            else:
-                if(record[2]=="-"):
-                    _N+=1
-                if(record[2]!="-"):
-                    if(target_N<=_N):
-                        print("故障サーバー:",_ip,", 故障期間:",_date,"~",record[0])
-                    _date=""
-                    _ip=""
-                    _N=0
+        # 別のサーバーのレコードを読み始めたときの初期化処理
+        if(record[1]!=_ip):
+            if(target_N<=_N):
+                print("故障サーバー:",_ip,", 故障中:",_fail_date,"~")
+            _over_date=""
+            _fail_date=""
+            _ip=record[1]
+            _N=0
+            _pngs=[]
+            
+        # サーバー落ち
         #　直前ログでサーバー故障してない場合
-        else:
+        if(_fail_date==""):
             if (record[2]=="-"):
-                _date=record[0];_ip=record[1];_N=1;
-    #　直前ログでサーバー故障している場合でログ終了した場合,ログ出力する
-    if(_ip!=""):
-        if(target_N<=_N):
-            print("故障サーバー:",_ip,", 故障中")
+                _server_failflag=True
+                _fail_date=record[0]
+                _N=1
+        #　直前ログでサーバー故障している場合
+        else:
+            if(record[2]=="-"):
+                _N+=1
+            if(record[2]!="-"):
+                if(target_N<=_N):
+                    print("故障サーバー:",_ip,", 故障期間:",_fail_date,"~",record[0])
+                _fail_date=""
+                _N=0
                 
+    #　直前ログでサーバー故障している場合でログ終了した場合,ログ出力する
+    if(target_N<=_N):
+        print("故障サーバー:",_ip,", 故障中",_fail_date,"~")
+            
+            
